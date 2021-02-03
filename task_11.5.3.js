3154ab3186fa1691018bd68e9a4600f340a5a05e
Alexey T <avtimakov70@gmail.com>
2021-02-03 20:03:40 +0700

    SF_Module11_Hometask: corrected task 11.5.3

--- task_11.5.3.js
+++ task_11.5.3.js
@@ -1,11 +1,16 @@
 function firstFunc(first){
     console.log(`First value - ${first}`);
     return function secondFunc(second){
-        console.log(`Second value - ${second}\nFirst + Second value - ${first + second}`);
+        console.log(`Second value - ${second}`);
+        return first + second
     }
 }
 resultFirstFunction = firstFunc(123);
-// console.log(resultFirstFunction);
-// console.log(resultFirstFunction());
-// console.log(resultFirstFunction(321));
-resultFirstFunction(321);
+
+// resultFirstFunction(321);
+
+// console.log(resultFirstFunction); - выведется код функции
+// console.log(resultFirstFunction()); - выведется undefined, т.к. не переданы параметры
+console.log(`First + Second value - ${resultFirstFunction(321)}`);
+
+// firstFunc(123)(321);
